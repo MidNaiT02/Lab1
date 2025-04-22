189b2f32cefe1043b3392cae0ed089af688e12c1
MidNaiT02 <aabil200601@gmail.com>
2025-04-22 14:12:48 +0500

    Delete PP2 lab11 directory

--- PP2 lab11/Ensure table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS PhoneBook (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    phone TEXT NOT NULL
-);
--- PP2 lab11/Search by name.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION find_contacts_by_pattern(pattern TEXT)
-RETURNS TABLE(id INT, name VARCHAR(100), phone VARCHAR(20)) AS $$
-BEGIN
-    RETURN QUERY 
-    SELECT id, name, phone
-    FROM contacts
-    WHERE name LIKE '%' || pattern || '%' OR phone LIKE '%' || pattern || '%';
-END;
-$$ LANGUAGE plpgsql;
